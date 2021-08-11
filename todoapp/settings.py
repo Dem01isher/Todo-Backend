134362a94601a8e7a12cab353fb56c2a33fc455f
Dem01sher <madarthas445@gmail.com>
2021-08-11 08:24:14 +0300

    Deploy project

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'USER': 'root',
         'PASSWORD':'',
         'HOST': '',
-        'PORT': '3306',
+        'PORT': '',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
         }
